434c403c151d6bad1aa629a56083c325eec3a744
Kirk Charles Niverba <kirkniverba23@gmail.com>
2021-07-22 22:15:34 +0800

    add tawk.to

---
+++ config/tawkto.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | tawk.to Direct Chat Link
+    |--------------------------------------------------------------------------
+    |
+    | The "Direct Chat Link" key from your admin dashboard at:
+    | https://dashboard.tawk.to/#/admin
+    |
+    |
+    */
+    'link' => env('TAWKTO_LINK'),
+
+];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
         @guest()
             @include('layouts.footers.guest')
         @endguest
-
+        {{ TawkTo::widgetCode() }}
         <script src="{{ asset('argon') }}/vendor/jquery/dist/jquery.min.js"></script>
         <script src="{{ asset('argon') }}/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
         
