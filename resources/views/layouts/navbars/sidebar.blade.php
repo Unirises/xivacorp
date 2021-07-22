4edd4c0ebf9efc57d190172a48c9c21af7864006
Kirk Charles Niverba <kirkniverba23@gmail.com>
2021-07-22 23:52:44 +0800

    fixes

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -77,6 +77,7 @@
                 </div>
             </form>
             <!-- Navigation -->
+            <!-- TODO: Do active navigation stuff -->
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('home') }}">
@@ -84,12 +85,12 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-examples">
+                    <a class="nav-link" href="#employees" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="employees">
                         <i class="ni ni-badge"></i>
                         <span class="nav-link-text">Employees</span>
                     </a>
 
-                    <div class="collapse show" id="navbar-examples">
+                    <div class="collapse hide" id="employees">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('profile.edit') }}">
@@ -105,12 +106,12 @@
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-examples">
+                    <a class="nav-link" href="#types" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="types">
                         <i class="ni ni-settings-gear-65"></i>
                         <span class="nav-link-text">Manage Types</span>
                     </a>
 
-                    <div class="collapse show" id="navbar-examples">
+                    <div class="collapse hide" id="types">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('profile.edit') }}">
@@ -126,12 +127,12 @@
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-examples">
+                    <a class="nav-link" href="#marketplace" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="marketplace">
                         <i class="ni ni-bag-17"></i>
                         <span class="nav-link-text">Marketplace</span>
                     </a>
 
-                    <div class="collapse show" id="navbar-examples">
+                    <div class="collapse hide" id="marketplace">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('profile.edit') }}">
@@ -147,12 +148,12 @@
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-examples">
+                    <a class="nav-link" href="#teleconsult" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="teleconsult">
                         <i class="ni ni-circle-08"></i>
                         <span class="nav-link-text">Teleconsult</span>
                     </a>
 
-                    <div class="collapse show" id="navbar-examples">
+                    <div class="collapse hide" id="teleconsult">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('profile.edit') }}">
@@ -168,12 +169,12 @@
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-examples">
+                    <a class="nav-link" href="#test_services" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="test_services">
                         <i class="ni ni-favourite-28"></i>
                         <span class="nav-link-text">Tests and Services</span>
                     </a>
 
-                    <div class="collapse show" id="navbar-examples">
+                    <div class="collapse hide" id="test_services">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('profile.edit') }}">
@@ -204,12 +205,12 @@
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-examples">
+                    <a class="nav-link" href="#forms" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="forms">
                         <i class="ni ni-collection"></i>
                         <span class="nav-link-text">Forms</span>
                     </a>
 
-                    <div class="collapse show" id="navbar-examples">
+                    <div class="collapse hide" id="forms">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('profile.edit') }}">
