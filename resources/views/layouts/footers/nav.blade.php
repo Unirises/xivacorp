e6882c7b2cca0fce6727ac1be66175505d053d0f
Kirk Charles Niverba <kirkniverba23@gmail.com>
2021-08-14 22:13:54 +0800

    add footer logos

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,3 +1,12 @@
 <div class="copyright text-center text-muted">
+    <h3 style="color: white">Powered By:</h3>
+    <img src="{{ asset('argon') }}/img/brand/vavs.png" style="max-width: 25%" alt=""><br><br>
+    <div class="d-flex justify-content-center" style="height: 50px">
+    <img src="{{ asset('argon') }}/img/brand/FDA.jpg" style="height: 100%" alt=""><br>
+        <img src="{{ asset('argon') }}/img/brand/DOH.jpg" style="height: 100%" alt=""><br>
+        <img src="{{ asset('argon') }}/img/brand/BNI.jpg" style="height: 100%" alt=""><br>
+    </div>  
+
+    <br>
     &copy; {{ now()->year }} <a href="https://xivahealth.io" class="font-weight-bold ml-1" target="_blank">xivahealth</a>
 </div>
