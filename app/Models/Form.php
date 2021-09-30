c974ad197d1e474d49f91e91741e5cc1354dc75f
Kirk Charles Niverba <kirkniverba23@gmail.com>
2021-09-30 20:37:17 +0800

    fix forms

--- app/Models/Form.php
+++ app/Models/Form.php
@@ -17,4 +17,19 @@ class Form extends Model
     {
         return Answer::where('user_id', auth()->user()->id)->where('form_id', $this->id)->exists();
     }
+
+    public function service_forms()
+    {
+        return $this->hasMany(ServiceForms::class, 'form_id', 'id');
+    }
+
+    public function consultation_forms()
+    {
+        return $this->hasMany(ConsultationForm::class, 'form_id', 'id');
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class, 'form_id', 'id');
+    }
 }
